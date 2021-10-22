489348898f48b8719b9c01bcbdc5a68d4d0b9d2d
RAHUL THANGAMANI <82194182+Rahul83326@users.noreply.github.com>
2021-10-22 10:58:08 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // Theme
+    theme: "darkSky",
+
+    // Email notifier account (sender)
+    host: "smtp.mailtrap.io", // Sender email smtp
+    port: "465", // Sender email port
+    user: "rahulthangamani2002@gmail.com", // Sender email username
+    pass: "8608340306", // Sender email password
+
+    // Your email to receive notification (receiver)  
+    from: 'rahulthangamani2002@gmail.com', // Sender email address
+    to: 'rahulthangamani2002@gmail.com', // Your email address
+    subject: 'Contact Us API', // Subject
+};
